3bbd5d2bbfc575c948e0b3e6136eeb7f9d005bf9
Festongithub <fkemboik@gmail.com>
2023-08-31 06:50:37 -0700

    Node-left , node-right

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left add a  node to the left
+ * @parent: parent to the  left-child
+ * @value : value to the node
+ * Return : NULL if it fails to exist
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+{
+	if (parent == NULL)
+		return (NULL);
+}
+new_node = binary_tree_node(parent, value);
+if (new_node == NULL)
+{
+	return (NULL);
+}
+
+if (parent->left != NULL)
+{
+	parent->left->parent = new_node;
+	new_node->left = parent->left;
+}
+
+parent->left = new_node;
+
+return (new_node);
+}
