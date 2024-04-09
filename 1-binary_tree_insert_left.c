efe790c1dd46610b75f4544aeb8ae2c3a03e0fbd
Festongithub <fkemboik@gmail.com>
2024-04-09 00:58:03 -0700

    Binary trees

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * *binary_tree_insert_left: inserts a node as the left-child of another node
+ * *binary_tree_insert_left-inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value:  is the value to store in the new node
  * Return: Always 0 (success)
@@ -17,8 +17,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
-                return (NULL);
-
+	{
+		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Adding new data =========>"
+git add .
+git commit -m "Binary trees"
+git push
