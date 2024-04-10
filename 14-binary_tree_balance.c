0d79477bfa31454522a9ff7e34005ebd8f327829
Festongithub <fkemboik@gmail.com>
2024-04-10 00:21:09 -0700

    Binary trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -22,13 +22,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                size_t r = 0, l = 0;
+	if (tree)
+	{
+		size_t r = 0, l = 0;
 
-                l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-                r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-                return ((l > r) ? l : r);
-        }
-        return (0);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
